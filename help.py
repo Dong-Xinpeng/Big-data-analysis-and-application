782d85674de3f952901a21f326456d467920d607
xp-dong <1457992388@qq.com>
2023-10-31 11:28:20 +0800

    check data

---
+++ help.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pickle 
+
+def check_query_emb():
+    temp = np.load("./data/query_emb.npy")
+    print("query_emb.npy")
+    print("type:",type(temp))
+    print("shape:",temp.shape)
+    print(temp[0])
+
+def check_labels_500():
+    file = open("./data/labels_500.pkl","rb")
+    print("labels_500.pkl")
+    temp  = pickle.load(file)
+    print("type:",type(temp))
+    print("shape:",len(temp),"*",len(temp[0]))
+    print(temp)
+
+
+# check_query_emb()
+# check_labels_500()
