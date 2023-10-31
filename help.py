49b15c77883fdbf0c50e763f43af5c7371175fa1
xp-dong <1457992388@qq.com>
2023-10-31 11:32:57 +0800

    check data

--- help.py
+++ help.py
@@ -3,19 +3,27 @@ import pickle
 
 def check_query_emb():
     temp = np.load("./data/query_emb.npy")
-    print("query_emb.npy")
+    print("## query_emb.npy")
     print("type:",type(temp))
     print("shape:",temp.shape)
-    print(temp[0])
+    # print(temp[0])
 
 def check_labels_500():
     file = open("./data/labels_500.pkl","rb")
-    print("labels_500.pkl")
+    print("## labels_500.pkl")
     temp  = pickle.load(file)
     print("type:",type(temp))
     print("shape:",len(temp),"*",len(temp[0]))
-    print(temp)
+    # print(temp)
+
+def check_gallery_emb():
+    temp = np.load("./data/gallery_emb.npy")
+    print("## gallery_emb.npy")
+    print("type:",type(temp))
+    print("shape:",temp.shape)
+    # print(temp[0])
 
 
-# check_query_emb()
-# check_labels_500()
+check_query_emb()
+check_labels_500()
+check_gallery_emb()
